f0494a39ddbc8152096116836182cc800ed0a05b
marklma <marklma@hotmail.com>
2014-04-19 19:35:16 +0800

    circle jplayer

--- js/main.js
+++ js/main.js
@@ -1,38 +1,13 @@
 //<![CDATA[
 $(document).ready(function() {
 
-    new jPlayerPlaylist({
-        jPlayer: "#jquery_jplayer_1",
-        cssSelectorAncestor: "#jp_container_1"
-    }, [{
-        title: "Cro Magnon Man",
-        mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+    var myCirclePlayer = new CirclePlayer("#jquery_jplayer_1", {
+        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
-        title: "Your Face",
-        mp3: "http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
-    }, {
-        title: "Cyber Sonnet",
-        mp3: "http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
-    }, {
-        title: "Tempered Song",
-        mp3: "http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
-    }, {
-        title: "Hidden",
-        mp3: "http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
-    }, {
-        title: "Thin Ice",
-        mp3: "http://www.jplayer.org/audio/mp3/Miaow-10-Thin-ice.mp3",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-10-Thin-ice.ogg"
-    }], {
+        cssSelectorAncestor: "#cp_container_1",
         swfPath: "js",
-        supplied: "oga, mp3",
         wmode: "window",
-        smoothPlayBar: true,
         keyEnabled: true
     });
 });
