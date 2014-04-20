d609c197c18e118cdc08b835f97e772df62c5434
marklma <marklma@hotmail.com>
2014-04-20 12:58:37 +0800

    change bgmusic

--- js/main.js
+++ js/main.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
         jPlayer: "#jquery_jplayer_1",
         cssSelectorAncestor: "#jp_container_1"
     }, [{
-        title: "Cro Magnon Man",
-        mp3: "dd.mp3"
+        title: "First Love",
+        mp3: "http://marklv.qiniudn.com/FirstLove.mp3",
+        oga: "http://marklv.qiniudn.com/FirstLove.ogg"
     }, {
-        title: "Cyber Sonnet",
+        title: "なきむし。",
         mp3: "//marklv.qiniudn.com/marklv1.mp3",
         oga: "//marklv.qiniudn.com/marklv1.ogg"
     }, {
-        title: "Your Face",
-        mp3: "d2.mp3"
+        title: "ddl",
+        mp3: "dd.mp3"
     }], {
         swfPath: "js",
         supplied: "mp3,oga"
