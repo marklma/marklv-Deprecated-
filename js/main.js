83270e298d4af0450bba59982e00bc43cb60b751
marklma <marklma@hotmail.com>
2014-04-20 23:39:21 +0800

    add bg music

--- js/main.js
+++ js/main.js
@@ -6,16 +6,25 @@ $(document).ready(function() {
         jPlayer: "#jquery_jplayer_1",
         cssSelectorAncestor: "#jp_container_1"
     }, [{
+        title: "Kiss The Rain",
+        mp3: "http://marklv.qiniudn.com/KissTheRain.mp3",
+        ogg: "http://marklv.qiniudn.com/KissTheRain.ogg"
+    }, {
+        title: "could this be love",
+        mp3: "http://marklv.qiniudn.com/couldthisbelove.mp3",
+        ogg: "http://marklv.qiniudn.com/couldthisbelove.ogg"
+    }, {
         title: "First Love",
         mp3: "http://marklv.qiniudn.com/FirstLove.mp3",
-        oga: "http://marklv.qiniudn.com/FirstLove.ogg"
+        ogg: "http://marklv.qiniudn.com/FirstLove.ogg"
     }, {
         title: "なきむし。",
         mp3: "http://marklv.qiniudn.com/marklv1.mp3",
-        oga: "http://marklv.qiniudn.com/marklv1.ogg"
+        ogg: "http://marklv.qiniudn.com/marklv1.ogg"
     }, {
-        title: "ddl",
-        mp3: "dd.mp3"
+        title: "安妮的仙境",
+        mp3: "http://marklv.qiniudn.com/annidexianjing.mp3",
+        ogg: "http://marklv.qiniudn.com/annidexianjing.ogg"
     }], {
         playlistOptions: {
             autoPlay: true
