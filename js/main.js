76672142f0b1c16abf1133927bf37b07c5259a68
marklma <marklma@homtail.com>
2014-04-21 10:44:35 +0800

    fix jplayer import err

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
 //<![CDATA[
 $(document).ready(function() {
     var jcp_pause = $(".jp-pause");
-    var jcp_play = $(".jp-play");
     new jPlayerPlaylist({
         jPlayer: "#jquery_jplayer_1",
         cssSelectorAncestor: "#jp_container_1"
     }, [{
         title: "Kiss The Rain",
         mp3: "http://marklv.qiniudn.com/KissTheRain.mp3",
-        ogg: "http://marklv.qiniudn.com/KissTheRain.ogg"
+        oga: "http://marklv.qiniudn.com/KissTheRain.ogg"
     }, {
         title: "could this be love",
         mp3: "http://marklv.qiniudn.com/couldthisbelove.mp3",
-        ogg: "http://marklv.qiniudn.com/couldthisbelove.ogg"
+        oga: "http://marklv.qiniudn.com/couldthisbelove.ogg"
     }, {
         title: "First Love",
         mp3: "http://marklv.qiniudn.com/FirstLove.mp3",
-        ogg: "http://marklv.qiniudn.com/FirstLove.ogg"
+        oga: "http://marklv.qiniudn.com/FirstLove.ogg"
     }, {
         title: "なきむし。",
         mp3: "http://marklv.qiniudn.com/marklv1.mp3",
-        ogg: "http://marklv.qiniudn.com/marklv1.ogg"
+        oga: "http://marklv.qiniudn.com/marklv1.ogg"
     }, {
         title: "安妮的仙境",
         mp3: "http://marklv.qiniudn.com/annidexianjing.mp3",
-        ogg: "http://marklv.qiniudn.com/annidexianjing.ogg"
+        oga: "http://marklv.qiniudn.com/annidexianjing.ogg"
     }], {
         playlistOptions: {
             autoPlay: true
         },
         swfPath: "js",
-        supplied: "mp3,ogg",
+        supplied: "mp3, oga",
         preload: "auto",
-        wmode: "window",
-        solution: "flash,html"
+        wmode: "window"
     });
 });
 //]]>
