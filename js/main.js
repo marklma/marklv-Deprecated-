f18183093804080881da8b3e17813e950c7a4e61
marklma <marklma@hotmail.com>
2014-04-27 21:02:29 +0800

    jc next prev fix

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 //<![CDATA[
 $(function() {
-    new jPlayerPlaylist({
-        jPlayer: "#jquery_jplayer_1",
-        cssSelectorAncestor: "#jp_container_1"
-    }, [{
-        title: "Kiss The Rain",
-        mp3: "http://marklv.qiniudn.com/resource/KissTheRain.mp3",
-        oga: "http://marklv.qiniudn.com/resource/KissTheRain.ogg"
-    }, {
-        title: "could this be love",
-        mp3: "http://marklv.qiniudn.com/resource/couldthisbelove.mp3",
-        oga: "http://marklv.qiniudn.com/resource/couldthisbelove.ogg"
-    }, {
-        title: "First Love",
-        mp3: "http://marklv.qiniudn.com/resource/FirstLove.mp3",
-        oga: "http://marklv.qiniudn.com/resource/FirstLove.ogg"
-    }, {
-        title: "なきむし。",
-        mp3: "http://marklv.qiniudn.com/resource/marklv1.mp3",
-        oga: "http://marklv.qiniudn.com/resource/marklv1.ogg"
-    }, {
-        title: "安妮的仙境",
-        mp3: "http://marklv.qiniudn.com/resource/annidexianjing.mp3",
-        oga: "http://marklv.qiniudn.com/resource/annidexianjing.ogg"
-    }], {
-        playlistOptions: {
-            autoPlay: true
-        },
-        swfPath: "js",
-        supplied: "mp3, oga",
-        preload: "auto",
-        wmode: "window"
-    });
+  new jPlayerPlaylist({
+    jPlayer: "#jquery_jplayer_1",
+    cssSelectorAncestor: "#jp_container_1"
+  }, [{
+    title: "Kiss The Rain",
+    mp3: "http://marklv.qiniudn.com/resource/KissTheRain.mp3",
+    oga: "http://marklv.qiniudn.com/resource/KissTheRain.ogg"
+  }, {
+    title: "could this be love",
+    mp3: "http://marklv.qiniudn.com/resource/couldthisbelove.mp3",
+    oga: "http://marklv.qiniudn.com/resource/couldthisbelove.ogg"
+  }, {
+    title: "First Love",
+    mp3: "http://marklv.qiniudn.com/resource/FirstLove.mp3",
+    oga: "http://marklv.qiniudn.com/resource/FirstLove.ogg"
+  }, {
+    title: "なきむし。",
+    mp3: "http://marklv.qiniudn.com/resource/marklv1.mp3",
+    oga: "http://marklv.qiniudn.com/resource/marklv1.ogg"
+  }, {
+    title: "安妮的仙境",
+    mp3: "http://marklv.qiniudn.com/resource/annidexianjing.mp3",
+    oga: "http://marklv.qiniudn.com/resource/annidexianjing.ogg"
+  }], {
+    playlistOptions: {
+      autoPlay: false
+    },
+    swfPath: "js",
+    supplied: "mp3, oga",
+    preload: "auto",
+    wmode: "window"
+  });
 
-    $('[data-jcarousel]').each(function() {
-        var el = $(this);
-        el.jcarousel(el.data());
-    });
-    $('[data-jcarousel-control]').each(function() {
-        var el = $(this);
-        el.jcarouselControl(el.data());
-    });
+  $('[data-jcarousel]').each(function() {
+    var el = $(this);
+    el.jcarousel(el.data());
+  });
+  $('[data-jcarousel-control]').each(function() {
+    var el = $(this);
+    el.jcarouselControl(el.data());
+  });
 });
 //]]>
