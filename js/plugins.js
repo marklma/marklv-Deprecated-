69c47dc6ef4d34603a5d086aea519e9d8d11975c
marklma <marklma@homtail.com>
2014-05-06 14:45:23 +0800

    loading init

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,40 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+  var method;
+  var noop = function() {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
     }
+  }
 }());
 
 // Place any jQuery/helper plugins in here.
+$(function() {
+  starter = $("#starter");
+  var clearLoading = setTimeout(function() {
+    starter.animate({
+        height: 0,
+        opacity: 0
+      },
+      "slow", function() {
+        starter.empty().remove();
+        shapeblue_jq = null;
+        shapepink_jq = null;
+        starter = null;
+      });
+  }, 520);
+  window.onload = clearLoading;
+});
