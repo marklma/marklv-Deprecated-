8d6901da5a7706c3e1a85c9669fef92db511e319
marklma <marklma@hotmail.com>
2014-05-15 00:02:53 +0800

    faq

--- js/main.js
+++ js/main.js
@@ -1,60 +1,62 @@
 //<![CDATA[
 $(function() {
-  new jPlayerPlaylist({
-    jPlayer: "#jquery_jplayer_1",
-    cssSelectorAncestor: "#jp_container_1"
-  }, [{
-    title: "Kiss The Rain",
-    mp3: "http://marklv.qiniudn.com/resource/KissTheRain.mp3",
-    oga: "http://marklv.qiniudn.com/resource/KissTheRain.ogg"
-  }, {
-    title: "could this be love",
-    mp3: "http://marklv.qiniudn.com/resource/couldthisbelove.mp3",
-    oga: "http://marklv.qiniudn.com/resource/couldthisbelove.ogg"
-  }, {
-    title: "第一次",
-    mp3: "http://marklv.qiniudn.com/resource/firsttime.mp3",
-    oga: "http://marklv.qiniudn.com/resource/firsttime.ogg"
-  }, {
-    title: "First Love",
-    mp3: "http://marklv.qiniudn.com/resource/FirstLove.mp3",
-    oga: "http://marklv.qiniudn.com/resource/FirstLove.ogg"
-  }, {
-    title: "なきむし。",
-    mp3: "http://marklv.qiniudn.com/resource/marklv1.mp3",
-    oga: "http://marklv.qiniudn.com/resource/marklv1.ogg"
-  }, {
-    title: "安妮的仙境",
-    mp3: "http://marklv.qiniudn.com/resource/annidexianjing.mp3",
-    oga: "http://marklv.qiniudn.com/resource/annidexianjing.ogg"
-  }], {
-    playlistOptions: {
-      autoPlay: false
-    },
-    swfPath: "js",
-    supplied: "mp3, oga",
-    preload: "auto",
-    wmode: "window"
-  });
-  $('[data-jcarousel]').each(function() {
-    var el = $(this);
-    el.jcarousel(el.data());
-  });
-  $('[data-jcarousel-control]').each(function() {
-    var el = $(this);
-    el.jcarouselControl(el.data());
-  });
-  //更换背景图片
-  var mainWrapper_jq = $("#main-wrapper");
-  var bgcount = 0;
-  var bgs = new Array('url(img/bg0.jpg)', 'url(img/bg1.jpg)');
+    new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    }, [{
+        title: "Kiss The Rain",
+        mp3: "http://marklv.qiniudn.com/resource/KissTheRain.mp3",
+        oga: "http://marklv.qiniudn.com/resource/KissTheRain.ogg"
+    }, {
+        title: "could this be love",
+        mp3: "http://marklv.qiniudn.com/resource/couldthisbelove.mp3",
+        oga: "http://marklv.qiniudn.com/resource/couldthisbelove.ogg"
+    }, {
+        title: "第一次",
+        mp3: "http://marklv.qiniudn.com/resource/firsttime.mp3",
+        oga: "http://marklv.qiniudn.com/resource/firsttime.ogg"
+    }, {
+        title: "First Love",
+        mp3: "http://marklv.qiniudn.com/resource/FirstLove.mp3",
+        oga: "http://marklv.qiniudn.com/resource/FirstLove.ogg"
+    }, {
+        title: "なきむし。",
+        mp3: "http://marklv.qiniudn.com/resource/marklv1.mp3",
+        oga: "http://marklv.qiniudn.com/resource/marklv1.ogg"
+    }, {
+        title: "安妮的仙境",
+        mp3: "http://marklv.qiniudn.com/resource/annidexianjing.mp3",
+        oga: "http://marklv.qiniudn.com/resource/annidexianjing.ogg"
+    }], {
+        playlistOptions: {
+            autoPlay: false
+        },
+        swfPath: "js",
+        supplied: "mp3, oga",
+        preload: "auto",
+        wmode: "window"
+    });
+    $('[data-jcarousel]').each(function() {
+        var el = $(this);
+        el.jcarousel(el.data());
+    });
+    $('[data-jcarousel-control]').each(function() {
+        var el = $(this);
+        el.jcarouselControl(el.data());
+    });
+    //love letter
+    $("#loveletter").load("loveletter.md");
+    //更换背景图片
+    var mainWrapper_jq = $("#main-wrapper");
+    var bgcount = 0;
+    var bgs = new Array('url(img/bg0.jpg)', 'url(img/bg1.jpg)', 'url(img/bg2.jpg)', 'url(img/bg3.jpg)');
 
-  function changeBg() {
-    bgcount++;
-    bgcount = bgcount % 2;
-    // alert(bgs[bgcount]);
-    mainWrapper_jq.css('background-image', bgs[bgcount]);
-  }
-  setInterval(changeBg, 30000);
+    function changeBg() {
+        bgcount++;
+        bgcount = bgcount % 4;
+        // alert(bgs[bgcount]);
+        mainWrapper_jq.css('background-image', bgs[bgcount]);
+    }
+    setInterval(changeBg, 30000);
 });
 //]]>
